6bd1ec1a7d8a16237f08e567307ca8228b0d2b0d
Georges Kovacs Ribeiro <gkovacsribeiro@gmail.com>
2014-03-21 16:53:39 -0300

    Simplificação do Double Ratio

--- src/main/scala/com/stocksimulator/common_strategies/DoubleRatioArbitrerStrategy.scala
+++ src/main/scala/com/stocksimulator/common_strategies/DoubleRatioArbitrerStrategy.scala
@@ -16,9 +16,10 @@ abstract class DoubleRatioArbitrerStrategy(market: Market, param: Parameters) ex
   lazy val roundUp = StrategyUtils.roundUpFactory(gran)
   lazy val roundDown = StrategyUtils.roundDownFactory(gran)
 
-  val elapsed: Int = ParamMaker("elapsed")
+ // val elapsed: Int = ParamMaker("elapsed")
+  val ratio: Double = ParamMaker("ratio")
   val spread_entrada: Int = ParamMaker("spread_entrada")
-  val spread_max: Int = ParamMaker("spread_max")
+  //val spread_max: Int = ParamMaker("spread_max")
   val timeToExit: Int = 0//par"time_exit"
 
  // val timeExitCallBack = new SimpleCallBack(timeToExit * 1000, timeExitCallBackExec)
@@ -26,7 +27,7 @@ abstract class DoubleRatioArbitrerStrategy(market: Market, param: Parameters) ex
   //windows <-- timeExitCallBack
 
 
-  lazy val mvAvg = createRatioFor2SymbolsMAvg(symbolA, symbolB, symbolC, elapsed / 1000, elapsed);
+  //lazy val mvAvg = createRatioFor2SymbolsMAvg(symbolA, symbolB, symbolC, elapsed / 1000, elapsed);
 
   def timeExitCallBackExec(): Unit = {
     //Log("Time is up! Exiting position...")
@@ -37,18 +38,18 @@ abstract class DoubleRatioArbitrerStrategy(market: Market, param: Parameters) ex
 
     val pos = getPosition(symbolA).quantity
     //if (Math.abs(pos) >= maxPos) -timeExitCallBack else +timeExitCallBack
-    if (mvAvg.isAvailable) {
-      Log(this.lastTick)
+    //if (mvAvg.isAvailable) {
+      //Log(this.lastTick)
       val infoPair = (getSymbol(symbolA), getSymbol(symbolB), getSymbol(symbolC))
       infoPair match {
         case (a: Quote, b: Quote, c: Quote) =>
           val midPr = midPrice(b)*midPrice(c)
-          val precoTeoricoA = mvAvg.lastVal * midPr
+          val precoTeoricoA = ratio * midPr//mvAvg.lastVal * midPr
 
-          val mod = (pos / maxPos) * (spread_max - spread_entrada)
+         // val mod = (pos / maxPos) * (spread_max - spread_entrada)
           val spread = spread_entrada
-          val spreadVenda = Math.max(spread - mod, spread)
-          val spreadCompra = Math.max(spread + mod, spread)
+          val spreadVenda = if(pos >0) 0 else spread 
+          val spreadCompra = if(pos <0) 0 else spread
 
           val buyPrice = roundDown(Math.min(a.bid.price, precoTeoricoA - spreadCompra))
           val sellPrice = roundUp(Math.max(a.ask.price, precoTeoricoA + spreadVenda));
@@ -62,7 +63,7 @@ abstract class DoubleRatioArbitrerStrategy(market: Market, param: Parameters) ex
 
         case _ => {}
       }
-    }
+    //}
   }
 
 }
