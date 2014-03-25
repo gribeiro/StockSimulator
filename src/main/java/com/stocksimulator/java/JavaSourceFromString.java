1762c4278c421e9acf0e5934fe0577db026e3ec6
Georges Kovacs Ribeiro <gkovacsribeiro@gmail.com>
2014-03-24 22:35:55 -0300

    Java Test

---
+++ src/main/java/com/stocksimulator/java/JavaSourceFromString.java
@@ -0,0 +1,40 @@
+package com.stocksimulator.java;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.Arrays;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject.Kind;
+
+public class JavaSourceFromString extends SimpleJavaFileObject {
+    /**
+     * The source code of this "file".
+     */
+    final String code;
+
+    /**
+     * Constructs a new JavaSourceFromString.
+     * @param name the name of the compilation unit represented by this file object
+     * @param code the source code for the compilation unit represented by this file object
+     */
+    public JavaSourceFromString(String name, String code) {
+        super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension),
+              Kind.SOURCE);
+       // System.out.println(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension));
+        this.code = code;
+    }
+
+    @Override
+    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+        return code;
+    }
+}
---
+++ src/test/scala/com/stocksimulator/JavaCompilerTest.scala
@@ -0,0 +1,20 @@
+package com.stocksimulator
+import com.stocksimulator.java_loader._
+import com.stocksimulator.debug.Log
+import org.scalatest._
+import Matchers._
+class CompilerTest extends FlatSpec { 
+ 
+  "A MemoryCompiler" should "Compile a hello world" in {
+    val stringToCompile = """
+      public class HelloWorld {
+    	public static void main(String args[]) {
+    	System.out.println("It Worked");
+       }
+      }
+      """
+     
+     val (_, success) = MemoryCompiler("HelloWorld", stringToCompile)
+     success should equal (true)
+  }
+}
